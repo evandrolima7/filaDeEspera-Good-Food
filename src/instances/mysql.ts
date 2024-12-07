21fe2ce080a5f3a927d5fa1d5b55957443385047
.browserslistrcEvandro <evandrodejesuslima@gmail.com>
2024-12-07 15:03:47 -0300

    Configuração do sequelize para o Heroku

--- src/instances/mysql.ts
+++ src/instances/mysql.ts
@@ -3,12 +3,25 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const sequelize = new Sequelize(
-    process.env.MYSQL_DB as string,
-    process.env.MYSQL_USER as string,
-    process.env.MYSQL_PASSWORD as string, {
+export const sequelize: Sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: "mysql",
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false, // Isso evita problemas de SSL no Heroku
+        },
+      },
+    })
+  : new Sequelize(
+      process.env.MYSQL_DB as string,
+      process.env.MYSQL_USER as string,
+      process.env.MYSQL_PASSWORD as string,
+      {
+        host: process.env.MYSQL_HOST,
+        port: parseInt(process.env.MYSQL_PORT as string),
         dialect: "mysql",
-        port: parseInt(process.env.MYSQL_PORT as string)
-    }
-);
+      }
+    );
 
+export default sequelize;
